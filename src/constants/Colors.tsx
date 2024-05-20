94dc478f7c718f893220def037a3885b1ceb558d
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-05-20 15:24:46 -0300

    feat: :building_construction: Create Structure Project and Configs

---
+++ src/constants/Colors.tsx
@@ -0,0 +1,14 @@
+export const Colors = {
+    theme: {
+        primary: "#191816",
+        secudary: "#FFC632",
+        tertiary: "#29292E",
+        fourthary: "#202024",
+    },
+    text: {
+        primary: "#D4CCB6",
+        secudary: "#AFB6C2",
+        white: "#ffffff",
+        yellow: "#FFC632",
+    },
+}
