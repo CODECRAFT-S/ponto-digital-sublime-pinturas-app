6942894ac66f5bb7a621ffe8274cd951b274aaff
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-05-21 16:17:43 -0300

    feat: :seedling: Started Create Component Button by App

---
+++ src/components/ButtonConfirm/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+
+import styles from "./styles";
+
+export default function ButtonConfirm() {
+    return(
+        <View
+        ></View>
+    )
+}
